1dc35152750cbb85862ff4b45c6b2b64a7be4149
Chen-tao <chentao2110334@vip.qq.com>
2017-06-18 10:32:24 +0800

    Create all_brew_install.sh

---
+++ brew/all_brew_install.sh
@@ -0,0 +1,6 @@
+# https://github.com/ggreer/the_silver_searcher
+brew install the_silver_searcher
+
+# https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+
